8e39803d71e09da5013d5f1c0bd12e5ec2d6f114
RajaDey1908 <raja.dey1908@gmail.com>
2019-10-18 20:33:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express=require('express');
+const jwt=require('jsonwebtoken');
+
+const app=express();
+
+app.get('/api', (req, res)=>{
+    res.json({
+        message: "welcome to the API"
+    });
+});
+
+app.post('/api/post', verifytoken, (req, res)=>{
+
+    jwt.verify(req.token, 'secretkey', (err, authData)=>{
+        if(err){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                messgae: "weclome to post api",
+                authData
+            });
+        }
+    })
+    
+});
+
+app.post('/api/login', (req, res)=>{
+    const user={
+        id:1,
+        name:'Raja',
+        email: 'raja@gmail.com'
+    }
+    
+
+    jwt.sign({user}, 'secretkey', {expiresIn: '30s'}, (err, token)=>{
+        res.json({
+            token
+        });
+    });
+});
+
+function verifytoken(req, res, next){
+//
+const bearerHeader= req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer= bearerHeader.split(' ');
+
+        const bearerToken= bearer[1];
+
+        req.token=bearerToken;
+
+        next();
+
+    }else{
+        res.sendStatus(403);
+    }
+}
+
+app.listen('5000', ()=> console.log("server is running in 5000"));
